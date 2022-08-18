6a23361ee87fbe3f2478b291c0bd2a5472a3c87e
lydia <105000328+lydiafadel@users.noreply.github.com>
2022-08-18 18:10:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 terraform {
-     cloud {
-    organization = "Hashi-sami"
-    workspaces {
-      name = "getting-started"
-    }
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
