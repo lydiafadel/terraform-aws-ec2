af30d478bad4b13e4341575463b6c72c940644a1
lydia <105000328+lydiafadel@users.noreply.github.com>
2022-08-18 14:39:29 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+     cloud {
+    organization = "Hashi-sami"
+    workspaces {
+      name = "getting-started"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-3"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-02d0b1ffa5f16402d"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-02b3ceae20b7230c7"	
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
